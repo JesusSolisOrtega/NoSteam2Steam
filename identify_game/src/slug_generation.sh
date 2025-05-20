8fed09ef802ba40ad84f795652b9dfd39e71efdf
Jesus Solis <52633262+JesusSolisOrtega@users.noreply.github.com>
2025-05-20 15:04:40 +0200

    Update slug_generation.sh

--- identify_game/src/slug_generation.sh
+++ identify_game/src/slug_generation.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Function to clean the name of the executable
 clean_exe_name() {
     local exe_name="$1"
     
@@ -18,7 +17,6 @@ clean_exe_name() {
     echo "$exe_name"
 }
 
-# Function to generate a slug from a given string
 generate_slug() {
     local name="$1"
     echo "$name" | iconv -t ascii//TRANSLIT | tr -cd '[:alnum:]' | tr '[:upper:]' '[:lower:]' | sed -e 's/[^a-z0-9]/-/g' -e 's/--*/-/g' -e 's/^-//' -e 's/-$//'
@@ -29,18 +27,15 @@ extract_slug() {
     exe_name=$(clean_exe_name "$exe_name")
     local slug
 
-    # Manejar números romanos (I, V, X) al final del nombre
     if [[ "$exe_name" =~ (I|V|X)+$ ]]; then
         local roman_part="${BASH_REMATCH[0]}"
         local base_name="${exe_name%$roman_part}"
         slug=$(echo "$base_name" | sed -E 's/([a-zA-Z])([A-Z])/\1-\2/g')
-        slug="${slug}-${roman_part,,}"  # Convertir números romanos a minúsculas
+        slug="${slug}-${roman_part,,}"  
     else
-        # Insertar guiones antes de mayúsculas no iniciales
         slug=$(echo "$exe_name" | sed -E 's/([a-zA-Z])([A-Z])/\1-\2/g')
     fi
 
-    # Convertir a minúsculas y normalizar
     slug=$(echo "$slug" | tr '[:upper:]' '[:lower:]' | tr -c '[:alnum:]\n' '-' | tr -s '-' | sed -E 's/^-|-$//g')
 
     echo "$slug"
@@ -51,13 +46,10 @@ extract_slug_without_hyphens() {
     exe_name=$(clean_exe_name "$exe_name")
     local slug
 
-    # Normalizar espacios, guiones bajos y caracteres especiales
     slug=$(echo "$exe_name" | tr '_' ' ' | tr -c '[:alnum:]\n' ' ')
 
-    # Convertir a minúsculas y eliminar puntuación
     slug=$(echo "$slug" | tr '[:upper:]' '[:lower:]' | tr -d '[:punct:]' | tr ' ' '-' | tr -s '-')
 
-    # Eliminar guiones dobles y guiones al inicio/final
     slug=$(echo "$slug" | sed -E 's/--+/-/g' | sed -E 's/^-|-$//g')
 
     echo "$slug"
@@ -69,14 +61,12 @@ try_different_slugs() {
     local slug_functions=("extract_slug" "extract_slug_without_hyphens")
     local slugs=()
 
-    # Generar todos los slugs primero
     for slug_function in "${slug_functions[@]}"; do
         local slug=$($slug_function "$exe_name")
-        log_data "Slug generado ($slug_function): $slug"
+        log_data "Slug generated ($slug_function): $slug"
         slugs+=("$slug")
     done
 
-    # Usar los slugs generados en la búsqueda
     for slug in "${slugs[@]}"; do
         combined_results=$(search_game "$slug")
         if [ "$(echo "$combined_results" | jq '.matches | length')" -gt 0 ]; then
@@ -85,7 +75,6 @@ try_different_slugs() {
         fi
     done
 
-    # Si no se encontraron resultados, intentar sin números romanos o normales
     if [[ "$exe_name" =~ (I|V|X)+$ || "$exe_name" =~ [0-9]+$ ]]; then
         local base_name="${exe_name%${BASH_REMATCH[0]}}"
         for slug_function in "${slug_functions[@]}"; do
