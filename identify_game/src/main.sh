a0d7996ac9ba1c4737e49bfdfe6552ba897b7dc6
Jesus Solis <52633262+JesusSolisOrtega@users.noreply.github.com>
2025-05-20 14:36:41 +0200

    English translated main.sh

--- identify_game/src/main.sh
+++ identify_game/src/main.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-# Cargar variables desde el archivo de configuración
 source "$(dirname "$0")/../variables.txt"
 
-# Importar funciones de otros scripts
 source "$(dirname "$0")/slug_generation.sh"
 source "$(dirname "$0")/config_extraction.sh"
 source "$(dirname "$0")/game_search.sh"
@@ -12,42 +10,32 @@ source "$(dirname "$0")/detect_game.sh"
 source "$(dirname "$0")/search_mode.sh"
 source "$(dirname "$0")/dialogs.sh"  
 
-# Función principal que orquesta la ejecución
 main() {
-    # Verificar dependencias
     check_dependencies
 
-    # Cargar datos existentes
     games_data=$(load_games_data)
 
-    # Seleccionar el archivo ejecutable
     local exe_path=$(select_exe_file)
 
-    # Verificar archivo
     if [ -f "$exe_path" ]; then
         while true; do
-            # Seleccionar el modo de búsqueda
             search_mode=$(select_search_mode)
 
-            if [ "$search_mode" == "Automática" ]; then
-                # Manejar búsqueda automática
+            if [ "$search_mode" == "Automatic" ]; then
                 if handle_automatic_search "$exe_path" "$games_data"; then
                     break
                 fi
             else
-                # Manejar búsqueda manual
                 if handle_manual_search "$exe_path" "$games_data"; then
                     break
                 fi
             fi
 
-            # Preguntar si desea intentar con otro nombre
             prompt_retry
         done
     else
-        echo "Archivo no válido. Inténtalo de nuevo."
+        echo "Invalid file. Try again."
     fi
 }
 
-# Ejecutar la función principal
 main
